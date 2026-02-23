ae2d5039fb5af35e2989858bbbc4d78ea7383892
Anvesh <anveshsahu15@gmail.com>
2026-02-23 17:21:12 +0530

    [Anvesh] : modified QuantityMeasurementApp class

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -34,16 +34,33 @@ public class QuantityMeasurementApp {
 //		System.out.println("Input: " + length5 + " and " + length6);
 //		System.out.println("Equal (" + demonstrateLengthEquality(length5, length6) + ")");
 
-		System.out.println(
-				"convert(1.0, FEET, INCHES) = " + demonstrateLengthConversion(1.0, LengthUnit.FEET, LengthUnit.INCHES));
-		System.out.println(
-				"convert(3.0, YARDS, FEET) = " + demonstrateLengthConversion(3.0, LengthUnit.YARDS, LengthUnit.FEET));
-		System.out.println("convert(36.0, INCHES, YARDS) = "
-				+ demonstrateLengthConversion(36.0, LengthUnit.INCHES, LengthUnit.YARDS));
-		System.out.println("convert(1.0, CENTIMETERS, INCHES) = "
-				+ demonstrateLengthConversion(1.0, LengthUnit.CENTIMETERS, LengthUnit.INCHES));
+//		System.out.println(
+//				"convert(1.0, FEET, INCHES) = " + demonstrateLengthConversion(1.0, LengthUnit.FEET, LengthUnit.INCHES));
+//		System.out.println(
+//				"convert(3.0, YARDS, FEET) = " + demonstrateLengthConversion(3.0, LengthUnit.YARDS, LengthUnit.FEET));
+//		System.out.println("convert(36.0, INCHES, YARDS) = "
+//				+ demonstrateLengthConversion(36.0, LengthUnit.INCHES, LengthUnit.YARDS));
+//		System.out.println("convert(1.0, CENTIMETERS, INCHES) = "
+//				+ demonstrateLengthConversion(1.0, LengthUnit.CENTIMETERS, LengthUnit.INCHES));
+//		Length yard = new Length(1.0, LengthUnit.YARDS);
+//		Length feet = new Length(3.0, LengthUnit.FEET);
+//		System.out.println("Equality check: " + demonstrateLengthEquality(yard, feet));
+
+		Length feet = new Length(1.0, LengthUnit.FEET);
+		Length inches = new Length(12.0, LengthUnit.INCHES);
+
+		Length result1 = feet.add(inches);
+		System.out.println("1 FEET + 12 INCHES = " + result1);
+
+		Length result2 = inches.add(feet);
+		System.out.println("12 INCHES + 1 FEET = " + result2);
+
 		Length yard = new Length(1.0, LengthUnit.YARDS);
-		Length feet = new Length(3.0, LengthUnit.FEET);
-		System.out.println("Equality check: " + demonstrateLengthEquality(yard, feet));
+		Length result3 = yard.add(new Length(3.0, LengthUnit.FEET));
+		System.out.println("1 YARD + 3 FEET = " + result3);
+
+		Length cm = new Length(2.54, LengthUnit.CENTIMETERS);
+		Length result4 = cm.add(new Length(1.0, LengthUnit.INCHES));
+		System.out.println("2.54 CM + 1 INCH = " + result4);
 	}
 }
