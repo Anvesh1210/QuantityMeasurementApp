ec5ff438a895edd75158cbf255a3bdd5724bc39f
Anvesh <anveshsahu15@gmail.com>
2026-03-12 16:08:08 +0530

    [Anvesh] : added model class for business logic representation of quantity

---
+++ src/main/java/com/apps/quantitymeasurement/model/QuantityModel.java
@@ -0,0 +1,29 @@
+package com.apps.quantitymeasurement.model;
+
+import com.apps.quantitymeasurement.core.IMeasurable;
+
+public class QuantityModel<U extends IMeasurable> {
+	public double value;
+	public U unit;
+
+	public QuantityModel(double value, U unit) {
+		this.value = value;
+		this.unit = unit;
+	}
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public U getUnit() {
+		return unit;
+	}
+
+	public void setUnit(U unit) {
+		this.unit = unit;
+	}
+
+}
